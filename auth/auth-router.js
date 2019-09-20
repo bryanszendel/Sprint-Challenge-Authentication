4ad09ceee2e94384dfd92d661d084f3821f658df
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-20 09:33:58 -0600

    POST /register endpoint is created

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,38 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets.js');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash;
+
+  Users.add(user)
+    .then(res => {
+      const token = generateToken(user)
+      res.status(201).json({res, token})
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error registering the user' })
+    })
 });
 
 router.post('/login', (req, res) => {
   // implement login
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;
--- users/users-model.js
+++ users/users-model.js
@@ -7,7 +7,7 @@ module.exports = {
   findById,
 }
 
-function add(user) {
+async function add(user) {
   const [id] = await db('users').insert(user)
   return findById(id)
 }
