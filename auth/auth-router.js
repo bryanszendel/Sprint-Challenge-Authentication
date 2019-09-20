639bd4f1467e8cc01fda573e997e325d8b0f18ac
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-20 09:51:56 -0600

    POST /login endpoint is created

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -10,10 +10,12 @@ router.post('/register', (req, res) => {
   let user = req.body
   const hash = bcrypt.hashSync(user.password, 10)
   user.password = hash;
+  console.log(user)
 
   Users.add(user)
     .then(res => {
       const token = generateToken(user)
+      console.log(token)
       res.status(201).json({res, token})
     })
     .catch(err => {
@@ -23,6 +25,20 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  let {username, password} = req.body
+
+  Users.findBy({ username }).first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({ message: `Welcome, ${user.username}.`, token })
+      } else {
+        res.status(401).json({ message: 'Invalid credentials.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 });
 
 function generateToken(user) {
